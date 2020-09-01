fd87dc5d6e5f5c8e4d06f29795cacb8f19f816ba
Daniel Mardones <dm.dev@outlook.com>
2020-09-01 14:58:23 -0400

    Fin práctica - SpotiApp

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  nuevasCanciones:any[] = [];
+  loading:boolean;
+  error: boolean;
+  mensajeError: string;
+
+
+  constructor( private spotify: SpotifyService) { 
+
+    this.loading = true;
+    this.error = false;
+
+    this.spotify.getNewReleases()
+        .subscribe(data => {
+          console.log(data);
+          this.nuevasCanciones = data;
+          this.loading = false;
+        }, ( errorServicio ) =>{
+          this.loading = false;
+          this.error = true;
+          this.mensajeError = errorServicio.error.error.message;
+        });
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
